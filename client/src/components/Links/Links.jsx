f57f87e7de0650a28734cb709661043c8fddbf74
Sam Berston <samuelberston@gmail.com>
2021-04-24 21:08:25 -0700

    icons

--- client/src/components/Links/Links.jsx
+++ client/src/components/Links/Links.jsx
@@ -9,13 +9,13 @@ const Links = () => (
     </div>
     <div id="links" className={css.links}>
       <div id="gitHub" className={css.link}>
-        GitHub
+        <i className="fa fa-github" />
       </div>
       <div id="linkedIn" className={css.link}>
-        LinkedIn
+        <i className="fa fa-linkedin-in" />
       </div>
       <div id="gmail" className={css.link}>
-        Gmail
+        <i className="fa fa-envelope-square" />
       </div>
     </div>
     <div id="bottomLine" className={`${css.line} ${css.bottomLine}`}>
