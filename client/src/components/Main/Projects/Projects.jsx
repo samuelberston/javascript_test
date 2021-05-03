d86e2c29098fc7ad16a2d5641dc8891d7f3f1334
Sam Berston <samuelberston@gmail.com>
2021-05-03 14:00:09 -0700

    format

--- client/src/components/Main/Projects/Projects.jsx
+++ client/src/components/Main/Projects/Projects.jsx
@@ -18,6 +18,10 @@ const Projects = () => (
       <br />
       Technologies: Google Maps API, Google Civic API, ES6, React, Nodejs, Express, MySQL,
       Jest/Enzyme/CircleCI
+      <div id="images">
+        <img src="https://media-exp1.licdn.com/dms/image/C562DAQGB59TGk5I8Ww/profile-treasury-image-shrink_800_800/0/1618297098474?e=1620162000&v=beta&t=jCV2RMWbfK1cMYLEn6CG9q-KudA5WbL0DSFHC2r3vHk" alt="" />
+        <img src="https://media-exp1.licdn.com/dms/image/C562DAQFHUzR2F_OEFA/profile-treasury-image-shrink_800_800/0/1618297068161?e=1620162000&v=beta&t=fyQ2Gkj48H4zCojeheX0152LkhlXZ83jqGCa97EzezY" alt="" />
+      </div>
     </div>
     <div className={css.textify}>
       <a className={css.textifyLink} href="https://github.com/samuelberston/Textify-App/blob/main/README.md" target="_blank" rel="noreferrer">
