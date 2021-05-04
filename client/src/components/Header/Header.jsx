86d4bd21b0090176471c9ee88c6fadf83e534f1d
Sam Berston <samuelberston@gmail.com>
2021-05-03 17:19:59 -0700

    contacts

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => (
     <div id="projects" className={css.module} role="button" onClick={() => { window.scrollTo({ top: 1550, behavior: 'smooth' }); }} onKeyPress={() => {}} tabIndex={0}>
       Projects
     </div>
-    <div id="contact" className={css.module}>
+    <div id="contact" className={css.module} role="button" onClick={() => { window.scrollTo({ top: 3000, behavior: 'smooth' }); }} onKeyPress={() => {}} tabIndex={0}>
       Contact
     </div>
     <a id={css.resume} className={css.module} href="https://www.linkedin.com/in/samberston/detail/overlay-view/urn:li:fsd_profileTreasuryMedia:(ACoAAB2itc4BGrbWkzmLTD4Tjs4U2eDL6KiJ0Ts,1635455824308)/" target="_blank" rel="noreferrer">
