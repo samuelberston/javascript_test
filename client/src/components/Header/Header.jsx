3e5fea9aced157301d3c467e3c3c66a106c75745
Sam Berston <samuelberston@gmail.com>
2021-05-10 15:25:16 -0700

    fixed scroll top

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -10,10 +10,10 @@ const Header = () => (
     <div id="technologies" className={css.module} role="button" onClick={() => { window.scrollTo({ top: 900, behavior: 'smooth' }); }} onKeyPress={() => {}} tabIndex={0}>
       Technologies
     </div>
-    <div id="projects" className={css.module} role="button" onClick={() => { window.scrollTo({ top: 1550, behavior: 'smooth' }); }} onKeyPress={() => {}} tabIndex={0}>
+    <div id="projects" className={css.module} role="button" onClick={() => { window.scrollTo({ top: 1650, behavior: 'smooth' }); }} onKeyPress={() => {}} tabIndex={0}>
       Projects
     </div>
-    <div id="contact" className={css.module} role="button" onClick={() => { window.scrollTo({ top: 3000, behavior: 'smooth' }); }} onKeyPress={() => {}} tabIndex={0}>
+    <div id="contact" className={css.module} role="button" onClick={() => { window.scrollTo({ top: 3200, behavior: 'smooth' }); }} onKeyPress={() => {}} tabIndex={0}>
       Contact
     </div>
     <a id={css.resume} className={css.module} href="https://www.linkedin.com/in/samberston/detail/overlay-view/urn:li:fsd_profileTreasuryMedia:(ACoAAB2itc4BGrbWkzmLTD4Tjs4U2eDL6KiJ0Ts,1635455824308)/" target="_blank" rel="noreferrer">
