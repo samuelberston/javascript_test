927677a57e0c182a4d7e14fc8ab4788dbb9fc5a2
Sam Berston <samuelberston@gmail.com>
2021-05-12 22:43:10 -0700

    resume

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => (
     <div id="contact" className={css.module} role="button" onClick={() => { window.scrollTo({ top: 3200, behavior: 'smooth' }); }} onKeyPress={() => {}} tabIndex={0}>
       Contact
     </div>
-    <a id={css.resume} className={css.module} href="https://www.linkedin.com/in/samberston/detail/overlay-view/urn:li:fsd_profileTreasuryMedia:(ACoAAB2itc4BGrbWkzmLTD4Tjs4U2eDL6KiJ0Ts,1635455824308)/" target="_blank" rel="noreferrer">
+    <a id={css.resume} className={css.module} href="Samuel Berston Resume.pdf" target="_blank" rel="noreferrer">
       Resume
     </a>
   </nav>
