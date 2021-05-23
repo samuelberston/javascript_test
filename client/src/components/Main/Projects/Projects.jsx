44e5f15a6818a43568e3d75c99f4cba5ff4b593c
Sam Berston <samuelberston@gmail.com>
2021-05-22 20:26:01 -0700

    fec link

--- client/src/components/Main/Projects/Projects.jsx
+++ client/src/components/Main/Projects/Projects.jsx
@@ -54,6 +54,23 @@ const Projects = () => (
     <h2>
       Projects
     </h2>
+    <div className={`${css.fec} ${css.project}`}>
+      <div className={css.projectLink}>
+        <a href="http://18.218.15.0" target="_blank" rel="noreferrer">
+          Ecommerce Site
+        </a>
+      </div>
+      <div className={css.text}>
+        This modern, user-friendly ecommerce product page was built using complex React architecture
+        and REST API.
+        <br />
+        <br />
+        <div className={css.technologies}>
+          Technologies: React, CSS modules, Express, Jest/Enzyme/CircleCI
+        </div>
+      </div>
+      <Carousel imgs={fecImgs} />
+    </div>
     <div className={`${css.localVoice} ${css.project}`}>
       <div className={css.projectLink}>
         <a type="button" href="https://github.com/samuelberston/local-voice/blob/main/README.md" target="_blank" rel="noreferrer">
@@ -91,24 +108,6 @@ const Projects = () => (
       </div>
       <Carousel imgs={textifyImgs} />
     </div>
-    <div className={`${css.fec} ${css.project}`}>
-      <div className={css.projectLink}>
-        <a href="https://github.com/samuelberston/Ecommerce-Site/blob/master/README.md" target="_blank" rel="noreferrer">
-          Ecommerce Site
-        </a>
-      </div>
-      <div className={css.text}>
-        This modern, user-friendly ecommerce product page was built using complex React architecture
-        and REST API.
-        <br />
-        <br />
-        <div className={css.technologies}>
-          Technologies: React, CSS modules, Express, Jest/Enzyme/CircleCI
-        </div>
-      </div>
-      <Carousel imgs={fecImgs} />
-    </div>
-
   </div>
 );
 
